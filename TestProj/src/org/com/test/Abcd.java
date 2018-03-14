9c642ae3282a7390a7dc87639c7c11a400387671
sumanta <sumantab4u2@gmail.com>
2018-03-14 11:38:49 +0530

    abcd comitted

---
+++ TestProj/src/org/com/test/Abcd.java
@@ -0,0 +1,27 @@
+package org.com.test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Abcd {
+
+	public static void main(String[] args) {
+		String [] currency = {"SGD", "USD", "INR", "GBP", "AUD", "SGD"};
+		System.out.println("Size of array: " + currency.length); 
+		List<String> currencyList = Arrays.asList(currency);
+		System.out.println(currencyList);
+		
+		List<String> assetList = new ArrayList();
+		String[] asset = {"equity", "stocks", "gold", "foriegn exchange", "fixed income", "futures", "options"};
+		Collections.addAll(assetList, asset);
+		System.out.println(assetList);
+System.out.println("");
+
+	    }
+}
+
+	
+
+
